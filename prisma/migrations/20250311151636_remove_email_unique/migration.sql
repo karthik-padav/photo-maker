bf3434ff0d8108ba2ade08ef0481bad74f6ce0a8
karthikpadav <karthikpadav@gmail.com>
2025-03-13 15:17:06 +0530

    debug

--- prisma/migrations/20250311151636_remove_email_unique/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `Account` DROP FOREIGN KEY `Account_userId_fkey`;
-
--- DropIndex
-DROP INDEX `Account_userId_key` ON `Account`;
-
--- AddForeignKey
-ALTER TABLE `Account` ADD CONSTRAINT `Account_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
